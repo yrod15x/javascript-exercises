c1549b8ef8993907ea88a3de2270312538d4b379
yrod15x <megasoldier15x@hotmail.com>
2024-05-13 19:13:19 -0400

    Ejercicio 02 Completado.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(palabra, numVeces) 
+{
+    let repetida = '';
+    if(numVeces < 0)
+    {
+        return 'ERROR';
+    }
+    for(let i = 0; i < numVeces; i++)
+    {
+        repetida += palabra;
+    }
 
+    return repetida;
 };
 
 // Do not edit below this line
