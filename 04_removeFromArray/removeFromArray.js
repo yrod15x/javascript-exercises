a2096d2bef5b06fde39543aa3445a38e21290128
yrod15x <megasoldier15x@hotmail.com>
2024-05-17 14:59:47 -0400

    Ejercicio 4 completado

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(lista, ...items) {
+    //los ... antes del ultimo argumento se usan para convertir todos los argumentos opcionales en un array
 
+    //Recorro el array hacia atras para que splice borre y corra el espacio borrado hacia la izquierda
+    for(let i = lista.length - 1; i >= 0 ; i--)
+    {
+        for(let j = 0; j < items.length; j++)
+        {
+            if(lista[i] === items[j])
+            {
+                lista.splice(i, 1);
+            }
+        }
+    }
+
+    return lista;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
