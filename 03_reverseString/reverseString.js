4e78444ddaf5e332cf3a99e57f52c3ea3d60d2c0
yrod15x <megasoldier15x@hotmail.com>
2024-05-17 11:29:12 -0400

    Ejercicio 3 completado.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(texto) {
+    let newTexto = '';
+    for(let i = texto.length - 1; i >= 0; i--)
+    {
+        newTexto += texto[i];
+    }
+    //console.log(newTexto);
+    return newTexto;
 };
+
+//reverseString('Hello');
 
 // Do not edit below this line
 module.exports = reverseString;
