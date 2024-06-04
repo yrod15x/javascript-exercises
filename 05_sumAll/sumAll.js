4a7555d35a5e24cb96d1e5ba84d5e668e0112e04
yrod15x <megasoldier15x@hotmail.com>
2024-06-04 13:44:31 -0400

    Ejercicio 05 Completado

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) 
+{
+    if(num1 < 0 || num2 < 0 || typeof(num1) != 'number' || typeof(num2) != 'number')
+    {
+        return 'ERROR';
+    }
+    if(num1 > num2)
+    {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    let suma = 0;
+    for(let i = num1; i <= num2; i++)
+    {
+        suma += i;
+    }
+    return suma;
 };
 
 // Do not edit below this line
