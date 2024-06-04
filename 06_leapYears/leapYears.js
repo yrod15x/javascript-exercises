2e6dcc39344b4c894565eab8108b8ddea2dc3ab0
yrod15x <megasoldier15x@hotmail.com>
2024-06-04 13:58:48 -0400

    Ejercicio 06 Completado.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(annio) 
+{
+    if(((annio % 4 == 0) && (annio % 100 != 0)) || (annio % 400 == 0))
+        {
+            return true;
+        }
+        return false;
 };
 
 // Do not edit below this line
