3d486c551f41c15d675a79b366baf650e5f22827
yrod15x <megasoldier15x@hotmail.com>
2024-06-18 17:55:05 -0400

    Ejercicio 7 completado.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) 
+{
+  let tempCelsius = (temp - 32) * 5 / 9;
+  if(tempCelsius % 2 != 0)
+    {
+      return Number(tempCelsius.toFixed(1));
+    }
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) 
+{
+  let tempFarenheit = (temp * 1.8) + 32;
+  if(tempFarenheit % 2 != 0)
+    {
+      return Number(tempFarenheit.toFixed(1));
+    }
+  return tempFarenheit;
 };
 
 // Do not edit below this line
