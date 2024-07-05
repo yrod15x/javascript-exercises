14ad4febac07380cd87b2a7f47fa7f4158dfe55f
yrod15x <megasoldier15x@hotmail.com>
2024-07-05 18:10:55 -0400

    Ejercicio 8 completado.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(numero1, numero2) 
+{
+  return numero1 + numero2;
 };
 
-const subtract = function() {
-	
+const subtract = function(numero1, numero2) 
+{
+	return numero1 - numero2;
 };
 
-const sum = function() {
-	
+const sum = function(numeros) 
+{
+	let suma = 0;
+  for(let i = 0; i < numeros.length; i++)
+  {
+    suma += numeros[i];
+  }
+  return suma;
 };
 
-const multiply = function() {
-
+const multiply = function(numeros) 
+{
+  let resultado = 1;
+  for(let i = 0; i < numeros.length; i++)
+  {
+    resultado *= numeros[i];
+  }
+  return resultado;
 };
 
-const power = function() {
-	
+const power = function(base, exponente)
+{
+	return Math.pow(base, exponente);
 };
 
-const factorial = function() {
-	
+const factorial = function(numero)
+{
+	let resultado = 1;
+  for(let i = 1; i <= numero; i++)
+  {
+    resultado *= i;
+  }
+  return resultado;
 };
 
 // Do not edit below this line
