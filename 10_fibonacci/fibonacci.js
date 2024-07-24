6e11fd6faabc4571012fda56e09661ae21afab3f
yrod15x <megasoldier15x@hotmail.com>
2024-07-24 18:34:16 -0400

    Ejercicio 10 Terminado.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(numero) 
+{
+    numero = Number(numero);
+    if(numero === 0)
+    {
+        return 0;
+    }
+    else if(numero < 0)
+    {
+        return "OOPS";
+    }
+    
 
+    let fibonaccis = [1, 1];
+
+    for(let i = 1; i < numero; i++)
+    {
+        fibonaccis.push(fibonaccis[i] + fibonaccis[i - 1]);
+    }
+
+    return fibonaccis[numero - 1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
